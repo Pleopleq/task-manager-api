e01b3477892fc75b8c3652582c91e38a83838e69
Felix Anducho <felix.anducho74@gmail.com>
2021-01-22 17:36:18 -0600

    Add multer for upload an avatar image. Added DELETE route for avatar.

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,10 +1,37 @@
 const express = require("express")
+const multer = require("multer")
 const User = require("../models/User")
 const auth = require("../middleware/auth")
 const router = new express.Router() 
+const upload = multer({
+    limits: {
+        fieldSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if(!file.originalname.match(/\.(jpg|png|jpeg)$/)) {
+            return cb(new Error("File must be JPG or PNG."))
+        }
+ 
+        cb(undefined, true) 
+    }
+})
 
 router.get("/users/me", auth, (req, res) => {
     res.send(req.user)
+})
+
+router.post("/users/me/avatar", auth, upload.single("avatar") , async (req, res) => {
+    req.user.avatar = req.file.buffer
+    await req.user.save()
+    res.send();
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
+})
+
+router.delete("/users/me/avatar", auth, async (req, res) => {
+    req.user.avatar = undefined
+    await req.user.save()
+    res.send();
 })
 
 router.post("/users", async (req, res) => {
@@ -16,7 +43,7 @@ router.post("/users", async (req, res) => {
     } catch (error) {
         res.status(400).send(error)
     }
-})
+}) 
 
 router.post("/users/login", async (req, res) => {
     try {
