899bab62aae23573d4ae65e25ebedacaf0e0cc8e
Felix Anducho <felix.anducho74@gmail.com>
2021-01-22 18:10:40 -0600

    Serving the avatar image

--- src/routers/user.js
+++ src/routers/user.js
@@ -20,6 +20,20 @@ router.get("/users/me", auth, (req, res) => {
     res.send(req.user)
 })
 
+router.get("/users/:id/avatar", async (req,res) => {
+    try {
+        const user = await User.findById(req.params.id)
+        if(!user || !user.avatar){
+            throw new Error()
+        }
+
+        res.set("Content-Type", "image/jpg")
+        res.send(user.avatar)
+    } catch (error) {
+        res.status(404).send().end()
+    }
+})
+
 router.post("/users/me/avatar", auth, upload.single("avatar") , async (req, res) => {
     req.user.avatar = req.file.buffer
     await req.user.save()
